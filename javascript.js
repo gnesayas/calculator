0a23fad71dd9b1145757c55c42408aa22d287436
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-08-28 15:30:54 -0500

    Implement the equal button, and adjust other buttons accordingly

--- javascript.js
+++ javascript.js
@@ -4,8 +4,11 @@ const DIVISION_BY_ZERO_ERROR = 'Cannot divide by zero';
 const history = document.querySelector('.history');
 const display = document.querySelector('.display');
 
+let operator = '';
 let leftOperand = '';
+let rightOperand = '';
 let justClickedOperator = false;
+let justEvaluated = false;
 
 let digitCount = 1;
 
@@ -29,6 +32,7 @@ function processNumber() {
         digitCount++;
     }
     justClickedOperator = false;
+    rightOperand = '';
 }
 
 const sign = document.querySelector('.sign');
@@ -56,12 +60,16 @@ clear.addEventListener('click', () => {
     history.textContent = '';
     display.textContent = 0;
     digitCount = 1;
-    currentOp = '';
+    operator = '';
+    leftOperand = '';
+    rightOperand = '';
+    justClickedOperator = false;
+    justEvaluated = false;
 });
 
 const back = document.querySelector('.back');
 back.addEventListener('click', () => {
-    if (justClickedOperator) return;
+    if (justClickedOperator || justEvaluated) return;
     if (display.textContent === DIVISION_BY_ZERO_ERROR ||
         display.textContent.length === 1 ||
         (display.textContent.length === 2 &&
@@ -83,21 +91,27 @@ const invert = document.querySelector('.invert');
 invert.addEventListener('click', () => {
     if (display.textContent === DIVISION_BY_ZERO_ERROR) return;
     display.textContent = divide(1, display.textContent);
+    leftOperand = display.textContent;
     justClickedOperator = false;
+    justEvaluated = true;
 });
 
 const square = document.querySelector('.square');
 square.addEventListener('click', () => {
     if (display.textContent === DIVISION_BY_ZERO_ERROR) return;
     display.textContent = multiply(display.textContent, display.textContent);
+    leftOperand = display.textContent;
     justClickedOperator = false;
+    justEvaluated = true;
 });
 
 const root = document.querySelector('.root');
 root.addEventListener('click', () => {
     if (display.textContent === DIVISION_BY_ZERO_ERROR) return;
     display.textContent = Math.sqrt(display.textContent);
+    leftOperand = display.textContent;
     justClickedOperator = false;
+    justEvaluated = true;
 });
 
 const div = document.querySelector('.div');
@@ -112,25 +126,49 @@ sub.addEventListener('click', processOperation);
 const addition = document.querySelector('.addition');
 addition.addEventListener('click', processOperation);
 
+const eval = document.querySelector('.eval');
+eval.addEventListener('click', processEvaluation);
+
 function processOperation() {
     if (display.textContent === DIVISION_BY_ZERO_ERROR) return;
     let currentOp = this.textContent;
-    if (!history.textContent) {
+    if (justEvaluated || !history.textContent) {
+        operator = currentOp;
         leftOperand = display.textContent;
-        history.textContent = `${leftOperand} ${currentOp}`;
+        history.textContent = `${leftOperand} ${operator}`;
     } else if (!justClickedOperator) {
-        let operator = history.textContent.slice(-1);
-        display.textContent = operate(operator, leftOperand, display.textContent);
+        rightOperand = display.textContent;
+        display.textContent = operate(operator, leftOperand, rightOperand);
         if (display.textContent !== DIVISION_BY_ZERO_ERROR) {
+            operator = currentOp;
             leftOperand = display.textContent;
             history.textContent = `${display.textContent} ${currentOp}`;
         } else {
             history.textContent += ' 0 /';
         }
-    } else if (currentOp !== history.textContent.slice(-1)) {
+    } else if (operator !== currentOp) {
+        operator = currentOp;
         history.textContent = history.textContent.slice(0, -1) + currentOp;
     }
     justClickedOperator = true;
+    justEvaluated = false;
+}
+
+function processEvaluation() {
+    if (display.textContent === DIVISION_BY_ZERO_ERROR ||
+        !operator || !leftOperand) return;
+    if (!rightOperand || justClickedOperator) {
+        rightOperand = display.textContent;
+    }
+    display.textContent = operate(operator, leftOperand, rightOperand);
+    if (display.textContent !== DIVISION_BY_ZERO_ERROR) {
+        leftOperand = display.textContent;
+        history.textContent = `${leftOperand} ${operator} ${rightOperand}`;
+    } else {
+        history.textContent += ' 0 /';
+    }
+    justEvaluated = true;
+    justClickedOperator = false;
 }
 
 function operate(operator, num1, num2) {
